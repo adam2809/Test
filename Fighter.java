7834f238b0a196c3449271d7b6b623d8d81d3b12
adam2809 <adam.kule@gmail.com>
2018-03-18 16:20:24 +0100

    Added isDeafeated() method.

--- Fighter.java
+++ Fighter.java
@@ -1,9 +1,16 @@
 public class Fighter {
     public String name;
     public int health, damagePerAttack;
+    
     public Fighter(String name, int health, int damagePerAttack) {
         this.name = name;
         this.health = health;
         this.damagePerAttack = damagePerAttack;
+    }
+    public boolean isDefeated(){
+        if(health<=0){
+            return true;    
+        }
+        return false;
     }
 }
